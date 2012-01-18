502af0c0d347d86e972cc175ccba610bcf3c799d
michael4beardon <michael@beardon.com>
2012-01-18 19:47:31 +0000

    Apparently not all files were versioned. Also added error page.

---
+++ error.php
@@ -0,0 +1,60 @@
+<?php
+// No direct access.
+defined('_JEXEC') or die;
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+			<jdoc:include type="head" />
+			<!-- <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" /> -->
+			<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/fpp/css/960_24_col.css" type="text/css" media="screen,projection" />
+			<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/fpp/css/stylesheet.css" type="text/css" media="screen,projection" />
+			<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/fpp/css/jquery.treeview.css" type="text/css" media="screen,projection" />
+			<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/fpp/css/jquery-ui-1.8.6.css" type="text/css" media="screen,projection" />
+			<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/fpp/css/style.css" type="text/css" media="screen,projection" />
+			
+			<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+			<script type="text/javascript" src="/templates/fpp/js/prodinfo.js"></script>
+			<script type="text/javascript" src="/templates/fpp/js/jquery.treeview.js"></script>
+			<script type="text/javascript" src="/templates/fpp/js/jquery-ui-1.8.6.min.js"></script>
+	</head>
+
+	<body>
+	
+		<div id="bodyWrapper" class="container_24">
+			<div id="header" class="grid_24">
+				<div id="storeLogo">
+					<a href="/">
+						<img src="/templates/fpp/images/logo.png" alt="Ifsta" />
+					</a>
+				</div>
+				<div id="HeaderRight">
+					<jdoc:include type="modules" name="navbar" style="navbar" />
+					<div style="clear: both; float: right; margin-top: 35px;">
+						<jdoc:include type="modules" name="topright" style="nothing" />
+					</div>
+				</div>
+			</div>
+			
+			<div id="bodyContent" class="grid_16 push_4">
+				
+				<div class="error">
+					<h2><?php echo "Error ".$this->error->getCode()." : ".$this->error->getMessage() ?></h2>
+					<p>A nice error message should appear here. I'll let someone else write it. <br />
+					<a href="/">Return to Index</a></p>
+				</div>
+
+			</div>
+
+			
+			<div class="grid_24 footer">
+				<p align="center">Copyright &copy; 2012 <a href="http://ifsta.org/">Ifsta</a>
+				<br />Powered by <a href="http://www.beardon.com" target="_blank">Beardon Services, Inc.</a>
+				</p>
+			</div>
+		
+		
+		</div>
+
+	</body>
+</html>
---
+++ html/com_search/search/default.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @version		$Id: default.php 20196 2011-01-09 02:40:25Z ian $
+ * @package		Joomla.Site
+ * @subpackage	com_search
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+?>
+
+<div class="search<?php echo $this->pageclass_sfx; ?>">
+	<div class="ui-widget infoBoxContainer">
+	
+<?php //if ($this->params->get('show_page_heading', 1)) : ?>
+<div class="ui-widget-header ui-corner-top infoBoxHeading">
+<h1>
+
+</h1>
+</div>
+<?php //endif; ?>
+		<div class="ui-widget-content ui-corner-bottom infoBoxContents">
+<?php 
+	
+//echo $this->loadTemplate('form'); ?>
+<?php if ($this->error==null && count($this->results) > 0) :
+	echo $this->loadTemplate('results');
+else :
+	echo $this->loadTemplate('error');
+endif; 
+?>
+		</div>
+	</div>
+</div>
