4902657569596ae180f809f47a7a19fd9f57e202
michael4beardon <michael@beardon.com>
2012-02-01 17:31:50 +0000

    com_products - initial commit mod_products_categories - initial commit both working, no admin backend yet import-articles.py - updated for new db structure template updated slightly for these changes

--- html/com_content/category/blog.php
+++ html/com_content/category/blog.php
@@ -121,7 +121,7 @@ JHtml::addIncludePath(JPATH_COMPONENT.'/helpers');
 		</div>
 	<?php endif; ?>
 
-<?php if (($this->params->def('show_pagination', 1) == 1  || ($this->params->get('show_pagination') == 2)) && ($this->pagination->get('pages.total') > 1)) : ?>
+<?php if ((($this->params->def('show_pagination', 1) == 1  || ($this->params->get('show_pagination') == 2)) && ($this->pagination->get('pages.total') > 1)) || true) : ?>
 		<div class="pagination">
 						<?php  if ($this->params->def('show_pagination_results', 1)) : ?>
 						<p class="counter">
