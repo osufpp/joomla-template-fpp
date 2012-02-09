8555967d028b80f1dc3babdb6a6c1688a66680c9
michael4beardon <michael@beardon.com>
2012-02-09 21:35:18 +0000

    Cleaned up javascript

--- js/chapterlists.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $("div#chapterlist_content").hide();
-    $("#chapterlist_header").click(function() {
-        $("#chapterlist_content").toggle(600);
-    })
-
-});
--- js/prodinfo.js
+++ js/prodinfo.js
@@ -41,4 +41,9 @@ $(function(){
 
 	}
 
+    $("div#chapterlist_content").hide();
+    $("#chapterlist_header").click(function() {
+        $("#chapterlist_content").toggle(600);
+    })
+
 });
