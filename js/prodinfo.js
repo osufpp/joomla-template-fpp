abf1518b47dc85c177978ddf086f0e6ae19b7627
michael4beardon <michael@beardon.com>
2012-02-13 15:32:54 +0000

    made shop URL a var in prodinfo.js

--- js/prodinfo.js
+++ js/prodinfo.js
@@ -1,12 +1,13 @@
 $(function(){
 
 	var p = $("div.item-page div.product_info");
+    var link = "http://shop.test.ifsta.org";
 	if(p) {
 		p.find('.product_price').html("Loading...");
 		var pid = p.find('.prodid').text();
 		var oid = $('.oscid').text();
 		$.ajax({
-			url: 'http://192.168.1.146/rpeters/shop/ext/tbg_api/product_info.php?cb=?&prod_id='+pid,
+			url: link + '/shop/ext/tbg_api/product_info.php?cb=?&prod_id='+pid,
 			dataType: 'json',
 			timeout: 10000,
 			success: function(d) {
@@ -22,13 +23,13 @@ $(function(){
 					if(d.year) 		{ p.append('<div>Year: '+d.year+'</div>'); 		}
 					
 					if(d.availability) {
-						p.append('<form method="post" action="http://192.168.1.146/rpeters/shop/ext/tbg_api/buy_ifsta_product.php?prod_id='+pid+'">\
+						p.append('<form method="post" action="'+ link +'/shop/ext/tbg_api/buy_ifsta_product.php?prod_id='+pid+'">\
 									<input type="submit" class="cartbtn" value="Add to Cart" />\
 								</form>');
 					} else {
 						p.append('<span class="unavailable">Unavailable</span>');
 					}
-					p.append('<form method="get" action="http://192.168.1.146/rpeters/shop/ext/tbg_api/ifsta_product.php">\
+					p.append('<form method="get" action="'+ link +'/ext/tbg_api/ifsta_product.php">\
 									<input type="hidden" name="prod_id" value="'+pid+'" />\
 									<input type="submit" class="cartbtn" value="Product Info"/>\
 								</form>');
