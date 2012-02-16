b3c5e423b790edbf60a3d075432ab64179d14595
michael4beardon <michael@beardon.com>
2012-02-16 23:19:51 +0000

    Fixed info display in case item is unavailable. Fixes #1818

--- js/prodinfo.js
+++ js/prodinfo.js
@@ -15,20 +15,24 @@ $(function(){
 					p.html('Something went wrong.');
 				}
 				else {
-                    p.html('<strong>Price:</strong> <span>$'+d.price+'</span>');
+                    if(d.availability) {
+                        p.html('<strong>Price:</strong> <span>$'+d.price+'</span>');
+                    }
+                    else {
+                        p.html('<span class="unavailable">Unavailable</span>');
+                    }
+                    p.append('<div><strong>IFSTA Item Number:</strong> '+pid+'</div>');
                     if(d.publisher) { p.append('<div><strong>Publisher:</strong> '+d.publisher+'</div>'); 		}
-                    if(d.ISBN10) 	{ p.append('<div><strong>ISBN10:</strong> '+d.ISBN10+'</div>'); 	}
-					if(d.ISBN13) 	{ p.append('<div><strong>ISBN13:</strong> '+d.ISBN13+'</div>'); 	}
-					if(d.edition) 	{ p.append('<div><strong>Edition:</strong> '+d.edition+'</div>'); 		}
-					if(d.year) 		{ p.append('<div><strong>Year:</strong> '+d.year+'</div>'); 		}
-                    if(d.pages) 	{ p.append('<div><strong>Pages:</strong> '+d.pages+'</div>'); 	}
+                    if(d.ISBN10) 	{ p.append('<div><strong>ISBN10:</strong> '+d.ISBN10+'</div>'); 	        }
+					if(d.ISBN13) 	{ p.append('<div><strong>ISBN13:</strong> '+d.ISBN13+'</div>'); 	        }
+					if(d.edition) 	{ p.append('<div><strong>Edition:</strong> '+d.edition+'</div>'); 		    }
+					if(d.year) 		{ p.append('<div><strong>Year:</strong> '+d.year+'</div>'); 		        }
+                    if(d.pages) 	{ p.append('<div><strong>Pages:</strong> '+d.pages+'</div>'); 	            }
 
 					if(d.availability) {
 						p.append('<form method="post" action="'+ link +'/ext/tbg_api/buy_ifsta_product.php?prod_id='+pid+'">\
 									<input type="submit" class="cartbtn button" value="Add to Cart" />\
 								</form>');
-					} else {
-						p.append('<span class="unavailable">Unavailable</span>');
 					}
 					p.append('<form method="get" action="'+ link +'/ext/tbg_api/ifsta_product.php">\
 									<input type="hidden" name="prod_id" value="'+pid+'" />\
