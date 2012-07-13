70322a1a0554f517098d6a494aa1b435aacaacb4
michael4beardon <michael@beardon.com>
2012-07-13 18:14:48 +0000

    brought jQuery Slideshow into sync with the one in OSCommerce

--- jss/jquery.slideshow.js
+++ jss/jquery.slideshow.js
@@ -1,35 +1,22 @@
 (function($){
-	$.fn.slideshow = function(opts) {
-		
-		var opts = $.extend({
-			'interval':		5000
-		});
-		
-		var spd = 2500;
-		/*
-		var interval = setInterval(function(ul){
-			if(ul) { //this doesn't work in IE as of 8.
-				var lis = ul.find("li");
-				var copy = lis.last().clone();
-				lis.last().fadeOut(spd, function () {
-					$(this).remove();
-				});
-				ul.prepend(copy);
-			}
-			
-		}, opts.interval, this);
-		*/
-		this.everyTime(opts.interval, function(i) {
-				var lis = $(this).find("li");
-				var copy = lis.last().clone();
-				lis.last().fadeOut(spd, function () {
-					$(this).remove();
-				});
-				$(this).prepend(copy);
+    $.fn.slideshow = function(opts) {
 
-		});
-		
-		return this;
-	}
-	
+        var opts = $.extend({
+            interval:		5000,
+            spd: 2500
+        }, opts);
+
+        this.everyTime(opts.interval, function(i) {
+            var lis = $(this).find("li");
+            var copy = lis.last().clone();
+            lis.last().fadeOut(opts.spd, function () {
+                $(this).remove();
+            });
+            $(this).prepend(copy);
+
+        });
+
+        return this;
+    }
+
 })(jQuery);
