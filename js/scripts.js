20e7607e9727a03bc574625b8caa402b56aa7cd0
rwtnorton <rwtnorton@beardon.com>
2013-10-18 21:34:14 +0000

    Moved GO button up by two pixels.

--- js/scripts.js
+++ js/scripts.js
@@ -139,6 +139,10 @@ jQuery('.hidden').hide();
     var oldGoButton = jQuery('input[value=Go][type=submit]');
     console.log(oldGoButton.length);
     oldGoButton.replaceWith('<input type="image" src="/templates/fpp/images/GoButton.png" alt="Submit search button" class="ui-corner-right" />');
+    jQuery('#go-button').css('vertical-align', '2px');
+
+    // Add distance around lateral edges of myIFSTA icon to
+    // see menu bar borders.
     var myIconThingy = jQuery('.my_icon');
     myIconThingy.css('margin-right', '5px');
 });
