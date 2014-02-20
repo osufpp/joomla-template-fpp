9f03b23f412e886d60a5cace16efd061556d7325
rwtnorton <rwtnorton@beardon.com>
2014-02-20 18:29:28 +0000

    s/googleanalytics/doubleclick/sortakinda;

--- index.php
+++ index.php
@@ -21,6 +21,21 @@ defined('_JEXEC') or die;
             <script type="text/javascript" src="/templates/fpp/js/scripts.js"></script>
             <link rel="stylesheet" type="text/css" href="/templates/fpp/jss/jss.css" />
             <script type="text/javascript" src="/templates/fpp/js/GA_download_outbound_links.js"></script>
+      <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-11628960-12']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script');
+          ga.type = 'text/javascript';
+          ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(ga, s);
+        })();
+      </script>
+
 	</head>
 
 	<body>
